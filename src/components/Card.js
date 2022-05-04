8a5fd63946f8f738cbe94a53540fb7bd9ed28e2a
Bibek Chaudhary <kingbibekc@gmail.com>
2022-05-04 13:19:01 +0545

    card components styled

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,9 +1,47 @@
-import React from 'react'
+import { useContext } from 'react'
 import { GithubContext } from '../context/context'
 import styled from 'styled-components'
 import { MdBusiness, MdLocationOn, MdLink } from 'react-icons/md'
+
 const Card = () => {
-  return <h2>card component</h2>
+  const { githubUser } = useContext(GithubContext)
+  const {
+    avatar_url,
+    html_url,
+    login,
+    name,
+    company,
+    blog,
+    bio,
+    location,
+    twitter_username,
+  } = githubUser
+  return (
+    <Wrapper>
+      <header>
+        <img src={avatar_url} alt={name} />
+        <div>
+          <h4>{name}</h4>
+          <p>@{twitter_username || login}</p>
+        </div>
+        <a href={html_url}>follow</a>
+      </header>
+      <p className='bio'>{bio}</p>
+      <div className='links'>
+        <p>
+          <MdBusiness /> {company}
+        </p>
+        <p>
+          <MdLocationOn />
+          {location}
+        </p>
+        <a href={`https://$blog`}>
+          <MdLink />
+          {blog}
+        </a>
+      </div>
+    </Wrapper>
+  )
 }
 const Wrapper = styled.article`
   background: var(--clr-white);
