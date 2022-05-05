2c1878b9f10f85083f84d98120d2f2cedaa18c4e
Bibek Chaudhary <kingbibekc@gmail.com>
2022-05-05 17:13:26 +0545

    setting up authentication with auth0

--- src/index.js
+++ src/index.js
@@ -2,15 +2,19 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
-// import * ServiceWorker from './serviceWorker';
+import { Auth0Provider } from '@auth0/auth0-react'
 import { GithubProvider } from './context/context'
-// import {Auth0Provider} from '@auth0/auth0-react'
 
 ReactDOM.render(
   <React.StrictMode>
-    <GithubProvider>
-      <App />
-    </GithubProvider>
+    <Auth0Provider
+      domain={process.env.REACT_APP_SET_DOMAIN}
+      clientId={process.env.REACT_APP_SET_CLIENT_ID}
+      redirectUri={window.location.origin}>
+      <GithubProvider>
+        <App />
+      </GithubProvider>
+    </Auth0Provider>
   </React.StrictMode>,
   document.getElementById('root')
 )
